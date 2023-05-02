e48ea55b81e4324733c09f1538296bee5a93b912
Serpentarius13 <serpentarium13@mail.ru>
2023-05-02 18:31:31 +0300

    !

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${sans.className} ${serif.className}`}>
+      <body className={` ${serif.className} ${sans.className} font-sans`}>
         <Header />
         <Navbar />
         <main className="flex flex-col gap-[3.3rem] max-w-[100vw] overflow-hidden">
